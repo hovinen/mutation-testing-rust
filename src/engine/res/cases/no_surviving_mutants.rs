d0004d5c67d836f83f382a39fa1f7a3865af19d5
Bradford Hovinen <hovinen@gmail.com>
2020-09-28 20:05:02 +0200

    Add more instruction swap mutations

--- src/engine/res/cases/no_surviving_mutants.rs
+++ src/engine/res/cases/no_surviving_mutants.rs
@@ -2,11 +2,11 @@ fn compare(a: u32, b: u32) {
     if a >= b {
         return
     }
-    panic!("Should fail")
 }
 
 fn main() {
     let a = 0;
     let b = 0;
     compare(a, b);
+    panic!("Should fail")
 }
--- src/mutation/all_mutators.rs
+++ src/mutation/all_mutators.rs
@@ -2,7 +2,7 @@ use crate::mutation::instruction_swapping_mutator::InstructionSwappingMutator;
 use crate::mutation::Mutator;
 use parity_wasm::elements::Instruction;
 
-pub(crate) static ALL_MUTATORS: [&'static (dyn Mutator + Send + Sync + 'static); 12] = [
+pub(crate) static ALL_MUTATORS: [&'static (dyn Mutator + Send + Sync + 'static); 52] = [
     &InstructionSwappingMutator {
         original_instruction: Instruction::I32GeU,
         replacement_instruction: Instruction::I32GtU,
@@ -36,6 +36,104 @@ pub(crate) static ALL_MUTATORS: [&'static (dyn Mutator + Send + Sync + 'static);
         replacement_instruction: Instruction::I32LeS,
     },
     &InstructionSwappingMutator {
+        original_instruction: Instruction::I32Eq,
+        replacement_instruction: Instruction::I32Ne,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I32Ne,
+        replacement_instruction: Instruction::I32Eq,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I32Add,
+        replacement_instruction: Instruction::I32Sub,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I32Sub,
+        replacement_instruction: Instruction::I32Add,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I32Add,
+        replacement_instruction: Instruction::I32Mul,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I32Mul,
+        replacement_instruction: Instruction::I32Add,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I32And,
+        replacement_instruction: Instruction::I32Or,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I32Or,
+        replacement_instruction: Instruction::I32And,
+    },
+
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I64GeU,
+        replacement_instruction: Instruction::I64GtU,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I64LeU,
+        replacement_instruction: Instruction::I64LtU,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I64GtU,
+        replacement_instruction: Instruction::I64GeU,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I64LtU,
+        replacement_instruction: Instruction::I64LeU,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I64GeS,
+        replacement_instruction: Instruction::I64GtS,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I64LeS,
+        replacement_instruction: Instruction::I64LtS,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I64GtS,
+        replacement_instruction: Instruction::I64GeS,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I64LtS,
+        replacement_instruction: Instruction::I64LeS,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I64Eq,
+        replacement_instruction: Instruction::I64Ne,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I64Ne,
+        replacement_instruction: Instruction::I64Eq,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I64Add,
+        replacement_instruction: Instruction::I64Sub,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I64Sub,
+        replacement_instruction: Instruction::I64Add,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I64Add,
+        replacement_instruction: Instruction::I64Mul,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I64Mul,
+        replacement_instruction: Instruction::I64Add,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I64And,
+        replacement_instruction: Instruction::I64Or,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::I64Or,
+        replacement_instruction: Instruction::I64And,
+    },
+
+    &InstructionSwappingMutator {
         original_instruction: Instruction::F32Ge,
         replacement_instruction: Instruction::F32Gt,
     },
@@ -44,11 +142,76 @@ pub(crate) static ALL_MUTATORS: [&'static (dyn Mutator + Send + Sync + 'static);
         replacement_instruction: Instruction::F32Ge,
     },
     &InstructionSwappingMutator {
+        original_instruction: Instruction::F32Eq,
+        replacement_instruction: Instruction::F32Ne,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::F32Ne,
+        replacement_instruction: Instruction::F32Eq,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::F32Add,
+        replacement_instruction: Instruction::F32Sub,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::F32Sub,
+        replacement_instruction: Instruction::F32Add,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::F32Add,
+        replacement_instruction: Instruction::F32Mul,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::F32Mul,
+        replacement_instruction: Instruction::F32Add,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::F32Ceil,
+        replacement_instruction: Instruction::F32Floor,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::F32Floor,
+        replacement_instruction: Instruction::F32Ceil,
+    },
+
+    &InstructionSwappingMutator {
         original_instruction: Instruction::F64Ge,
         replacement_instruction: Instruction::F64Gt,
     },
     &InstructionSwappingMutator {
         original_instruction: Instruction::F64Gt,
         replacement_instruction: Instruction::F64Ge,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::F64Eq,
+        replacement_instruction: Instruction::F64Ne,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::F64Ne,
+        replacement_instruction: Instruction::F64Eq,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::F64Add,
+        replacement_instruction: Instruction::F64Sub,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::F64Sub,
+        replacement_instruction: Instruction::F64Add,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::F64Add,
+        replacement_instruction: Instruction::F64Mul,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::F64Mul,
+        replacement_instruction: Instruction::F64Add,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::F64Ceil,
+        replacement_instruction: Instruction::F64Floor,
+    },
+    &InstructionSwappingMutator {
+        original_instruction: Instruction::F64Floor,
+        replacement_instruction: Instruction::F64Ceil,
     },
 ];
