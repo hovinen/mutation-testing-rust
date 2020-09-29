917992a59a1b75a33e6b214ca23d7d5756219d0f
Bradford Hovinen <hovinen@gmail.com>
2020-09-29 22:07:54 +0200

    Support two drop instructions replacing a store

--- src/mutation/set_cancelling_mutator.rs
+++ src/mutation/set_cancelling_mutator.rs
@@ -7,7 +7,21 @@ pub(crate) struct SetCancellingMutator;
 
 impl Mutator for SetCancellingMutator {
     fn perform(&self, body: &mut FuncBody, index: usize) {
-        body.code_mut().elements_mut()[index] = Instruction::Drop;
+        match body.code_mut().elements_mut()[index] {
+            Instruction::SetLocal(_) | Instruction::SetGlobal(_) => {
+                body.code_mut().elements_mut()[index] = Instruction::Drop;
+            }
+            Instruction::I32Store(_, _)
+            | Instruction::I64Store(_, _)
+            | Instruction::F32Store(_, _)
+            | Instruction::F64Store(_, _) => {
+                body.code_mut().elements_mut()[index] = Instruction::Drop;
+                body.code_mut()
+                    .elements_mut()
+                    .insert(index, Instruction::Drop);
+            }
+            _ => {}
+        }
     }
 
     fn find(&self, body: &FuncBody, function_index: usize) -> Vec<Mutation> {
@@ -77,6 +91,30 @@ mod tests {
     }
 
     #[test]
+    fn perform_inserts_drop_instruction_where_required() {
+        let subject = SetCancellingMutator;
+        let mut body = FuncBodyBuilder::with_callback(Identity)
+            .with_instructions(Instructions::new(vec![
+                Instruction::GetLocal(0),
+                Instruction::I32Store(0, 0),
+                Instruction::GetLocal(0),
+            ]))
+            .build();
+
+        subject.perform(&mut body, 1);
+
+        assert_eq!(
+            body.code().elements(),
+            vec![
+                Instruction::GetLocal(0),
+                Instruction::Drop,
+                Instruction::Drop,
+                Instruction::GetLocal(0)
+            ]
+        );
+    }
+
+    #[test]
     fn find_does_not_identify_mutation_in_empty_function() {
         let subject = SetCancellingMutator;
         let body = FuncBodyBuilder::with_callback(Identity)
@@ -101,7 +139,7 @@ mod tests {
     }
 
     macro_rules! mutation_tests {
-        ($($name:ident: $instruction:expr,)*) => {
+        ($($name:ident: $instruction:expr, $replacement:expr,)*) => {
         mod find_identifies_mutation_index_0 {
             use crate::mutation::set_cancelling_mutator::SetCancellingMutator;
             use parity_wasm::builder::{FuncBodyBuilder, Identity, ModuleBuilder};
@@ -126,7 +164,7 @@ mod tests {
                     result[0].perform(&mut module);
                     assert_eq!(
                         module.code_section().unwrap().bodies()[0].code().elements(),
-                        vec![Instruction::Drop]
+                        $replacement
                     );
                 }
             )*
@@ -135,12 +173,12 @@ mod tests {
     }
 
     mutation_tests! {
-        set_local: Instruction::SetLocal(0),
-        set_global: Instruction::SetGlobal(0),
-        i32_store: Instruction::I32Store(0, 0),
-        i64_store: Instruction::I64Store(0, 0),
-        f32_store: Instruction::F32Store(0, 0),
-        f64_store: Instruction::F64Store(0, 0),
+        set_local: Instruction::SetLocal(0), vec![Instruction::Drop],
+        set_global: Instruction::SetGlobal(0), vec![Instruction::Drop],
+        i32_store: Instruction::I32Store(0, 0), vec![Instruction::Drop, Instruction::Drop],
+        i64_store: Instruction::I64Store(0, 0), vec![Instruction::Drop, Instruction::Drop],
+        f32_store: Instruction::F32Store(0, 0), vec![Instruction::Drop, Instruction::Drop],
+        f64_store: Instruction::F64Store(0, 0), vec![Instruction::Drop, Instruction::Drop],
     }
 
     #[test]
